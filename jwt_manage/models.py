dd38a065476d34d9b0949ebeb13ea21ca35f2f9e
Giri <Girisuryamawandi@gmail.com>
2024-07-18 12:46:50 +0700

    add jwt_manage

---
+++ jwt_manage/models.py
@@ -0,0 +1,26 @@
+# models.py
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+
+db = SQLAlchemy()
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(200), nullable=True)
+    done = db.Column(db.Boolean, default=False)
+
+    def __repr__(self):
+        return f"<Task {self.title}>"
+    
+    
+class User_jwt(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
+    
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
---
+++ jwt_manage/resources.py
@@ -0,0 +1,44 @@
+from flask_restful import Resource, reqparse
+from flask_jwt_extended import jwt_required
+from models import Task, db
+
+task_parser = reqparse.RequestParser()
+task_parser.add_argument('title', type=str, required=True, help='Title is required')
+task_parser.add_argument('description', type=str)
+task_parser.add_argument('done', type=bool)
+
+class TaskListResource(Resource):
+    def get(self):
+        tasks = Task.query.all()
+        return [{'id': task.id, 'title': task.title, 'description': task.description, 'done': task.done} for task in tasks]
+
+
+    def post(self):
+        args = task_parser.parse_args()
+        task = Task(title=args['title'], description=args['description'], done=args['done'])
+        db.session.add(task)
+        db.session.commit()
+        return {'id': task.id, 'title': task.title, 'description': task.description, 'done': task.done}, 201
+
+class TaskResource(Resource):
+    @jwt_required()
+    def get(self, task_id):
+        task = Task.query.get_or_404(task_id)
+        return {'id': task.id, 'title': task.title, 'description': task.description, 'done': task.done}
+
+    @jwt_required()
+    def put(self, task_id):
+        args = task_parser.parse_args()
+        task = Task.query.get_or_404(task_id)
+        task.title = args['title']
+        task.description = args['description']
+        task.done = args['done']
+        db.session.commit()
+        return {'id': task.id, 'title': task.title, 'description': task.description, 'done': task.done}
+
+    @jwt_required()
+    def delete(self, task_id):
+        task = Task.query.get_or_404(task_id)
+        db.session.delete(task)
+        db.session.commit()
+        return '', 204
